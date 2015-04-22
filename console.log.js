b125a1a27699f7d72a38f04bf5d10731692d977b
ismael-miguel <espertalhao04@hotmail.com>
2015-04-22 15:29:05 +0100

    Better recursion handling. console.clear() hookup
    Elements and the window object no longer crash the code in ugly ways. Added the handling of the `console.clear()` method to delete the messages that were relayed.

--- console.log.js
+++ console.log.js
@@ -1,176 +1,216 @@
-	(function (window, undefined)
+(function (window, undefined)
+{
+
+	window.console._RELAY_TO_DOC = false;
+	window.console._USE_JSON = false;
+	window.console._SHOW_COOL_NUMBERS = false;
+	window.console._SHOW_TYPES = false;
+	window.console._SHOW_STRING_LENGTH = false;
+
+	if (window.console.log.toString() != window.Math.log.toString() && window.console.warn)
 	{
+		window.console.warn('The current console.log() may not be the native method!');
+	}
 
-		window.console._RELAY_TO_DOC = false;
-		window.console._USE_JSON = false;
-		window.console._SHOW_COOL_NUMBERS = false;
-		window.console._SHOW_TYPES = false;
-		window.console._SHOW_STRING_LENGTH = false;
-
-		if (window.console.log.toString() != window.Math.log.toString() && window.console.warn)
+	var messages = [],
+		old_console_log = window.console.log,
+		escapeHTML = function (string)
 		{
-			window.console.warn('The current console.log() may not be the native method!');
-		}
-
-		var old_console_log = window.console.log,
-			escapeHTML = function (string)
+			return string.replace(/[&<>"'\/]/g, function (c)
 			{
-				return string.replace(/[&<>"'\/]/g, function (c)
-				{
-					return {
-						'&': '&amp;',
-						'<': '&lt;',
-						'>': '&gt;',
-						'"': '&quot;',
-						'\'': '&#39;',
-						'/': '&#x2F;'
-					}[c];
-				});
-			},
-			isEmpty = function (o)
+				return {
+					'&': '&amp;',
+					'<': '&lt;',
+					'>': '&gt;',
+					'"': '&quot;',
+					'\'': '&#39;',
+					'/': '&#x2F;'
+				}[c];
+			});
+		},
+		isEmpty = function (o)
+		{
+			for (var k in o)
 			{
-				for (var k in o)
+				return false;
+			}
+			return true;
+		},
+		format = function (string)
+		{
+			return string.replace(/%([difs])(?:\.(\d+))?/g, function (all, type, size)
+			{
+				if ((i + 1) < args.length)
 				{
-					return false;
+					i++;
 				}
-				return true;
-			},
-			format = function (string)
-			{
-				return string.replace(/%([difs])(?:\.(\d+))?/g, function (all, type, size)
-				{
-					if ((i + 1) < args.length)
-					{
-						i++;
-					}
 
-					switch (type)
+				switch (type)
+				{
+				case 's':
+					return args[i] + '';
+				case 'd':
+				case 'i':
+					return (args[i] / 1).toFixed(0);
+				case 'f':
+					if (size)
 					{
-					case 's':
-						return args[i] + '';
-					case 'd':
-					case 'i':
-						return (args[i] / 1).toFixed(0);
-					case 'f':
-						if (size)
+						return (args[i] / 1).toFixed(size);
+					}
+					else
+					{
+						return args[i] / 1;
+					}
+				}
+			});
+		},
+
+		args, i,
+		pre_style = 'border:none;background:white;color:black;margin:0;padding:3px 0px 0px 3px;font-family:monospace;word-wrap:break-word;',
+
+		generateHTML = function (args, sub, recursion)
+		{
+
+			switch (typeof args)
+			{
+			case 'undefined':
+			case 'boolean':
+			case 'function':
+				return (window.console._SHOW_TYPES ? '(<span style="color:#00F">' + (typeof args) + '</span>) ' : '') + escapeHTML(args ? args.toString() : 'undefined');
+			case 'number':
+				return (window.console._SHOW_TYPES ? '(<span style="color:#00F">number</span>) ' : '') + args + (window.console._SHOW_COOL_NUMBERS && args == args ? ' <span style="color:#777">(0x' + args.toString(16) + ', 0o' + args.toString(8) + ', 0b' + args.toString(2) + ')</span>' : '');
+			case 'string':
+				var tmp = format(args.toString());
+				return (window.console._SHOW_TYPES ? '(<span style="color:#00F">string</span>) ' : '') + '"' + escapeHTML(tmp) + '"' + (window.console._SHOW_STRING_LENGTH ? ' <span style="color:#777">(length: ' + tmp.length + ')</span>' : '');
+			case 'object':
+				if (args == null)
+				{
+					return (window.console._SHOW_TYPES ? '(<span style="color:#00F">object</span>) ' : '') + 'null';
+				}
+				else if (args instanceof window.String)
+				{
+					return '[object String]: "' + escapeHTML(args) + '"' + (window.console._SHOW_STRING_LENGTH ? ' <span style="color:#777">(length: ' + args.length + ')</span>' : '');
+				}
+				else if (args instanceof window.RegExp)
+				{
+					return '[object RegExp]: ' + escapeHTML(args.toString());
+				}
+				else if (isEmpty(args))
+				{
+					var tmp;
+					return window.Object.prototype.toString.call(args) + (
+						args.valueOf ? ': ' + ((tmp = args.valueOf()) == args && (typeof tmp == 'object') ? ('length' in tmp ? '[]' : '{}') : tmp) : ''
+					);
+				}
+				else
+				{
+					var html = '<div style="display:block;color:black;background:white;"><label style="cursor:pointer;"><span style="font-family:sans-serif;font-size:13px;">&#9654;</span><input type="checkbox" style="display:none;" onchange="this.parentNode.parentNode.getElementsByTagName(\'div\')[0].style.display=this.checked?\'block\':\'none\';this.parentNode.getElementsByTagName(\'span\')[0].innerText=this.checked?\'&#9660;\':\'&#9654;\';">' + Object.prototype.toString.call(args) + '</label><div style="width:100%;border:1px solid #999;border-right:0px;' + (sub ? 'border-bottom:0px;' : '') + 'padding-left:6px;box-sizing:border-box;display:none;">';
+
+					for (var k in args)
+					{
+						if(recursion)
 						{
-							return (args[i] / 1).toFixed(size);
+							html += '<pre style="' + pre_style + '"><span style="color:#F00;">' + k + '</span>: ' + generateHTML(args[k], true, !(args instanceof Element) && args.self!=args) + '</pre>';
 						}
 						else
 						{
-							return args[i] / 1;
+							if((typeof args[k])=='object')
+							{
+								html += '<pre style="' + pre_style + '"><span style="color:#F00;">' + k + '</span>: ' + (args[k]||'null').toString() + '</pre>';
+							}
+							else
+							{
+								html += '<pre style="' + pre_style + '"><span style="color:#F00;">' + k + '</span>: ' + generateHTML(args[k], true, false) + '</pre>';
+							}
+							
 						}
 					}
-				});
-			},
 
-			args, i,
-			pre_style = 'background:white;color:black;margin:0;padding:3px 0px 0px 3px;font-family:monospace;word-wrap:break-word;',
+					return html + '</div></div>';
+				}
+			}
 
-			generateHTML = function (args, sub)
+		},
+
+		redirect_log = window.console.log = function ()
+		{
+
+			old_console_log.apply(window.console, arguments);
+
+			if (window.console._RELAY_TO_DOC)
 			{
+				var div = window.document.createElement('div');
 
-				switch (typeof args)
+				messages[messages.length] = div;
+				
+				args = arguments;
+
+				div.style.border = '1px solid #333';
+				div.style.background = '#666';
+				div.className = 'console-log';
+				div.innerHTML = '<span style="color:#CDCDCD;font-family:sans-serif;font-size:13px;">Console message at ' + (new Date()) + '</span><b onclick="this.parentNode.parentNode.removeChild(this.parentNode)" style="float:right;color:#CDCDCD;cursor:pointer;">&times;</b>';
+
+
+				for (i = 0, l = args.length; i < l; i++)
 				{
-				case 'undefined':
-				case 'boolean':
-				case 'function':
-					return (window.console._SHOW_TYPES ? '(<span style="color:#00F">' + (typeof args) + '</span>) ' : '') + args.toString();
-				case 'number':
-					return (window.console._SHOW_TYPES ? '(<span style="color:#00F">number</span>) ' : '') + args + (window.console._SHOW_COOL_NUMBERS && args == args ? ' <span style="color:#777">(0x' + args.toString(16) + ', 0o' + args.toString(8) + ', 0b' + args.toString(2) + ')</span>' : '');
-				case 'string':
-					var tmp = format(args.toString());
-					return (window.console._SHOW_TYPES ? '(<span style="color:#00F">string</span>) ' : '') + '"' + escapeHTML(tmp) + '"' + (window.console._SHOW_STRING_LENGTH ? ' <span style="color:#777">(length: ' + tmp.length + ')</span>' : '');
-				case 'object':
-					if (args == null)
-					{
-						return (window.console._SHOW_TYPES ? '(<span style="color:#00F">object</span>) ' : '') + 'null';
-					}
-					else if (args instanceof window.String)
-					{
-						return '[object String]: "' + escapeHTML(args) + '"' + (window.console._SHOW_STRING_LENGTH ? ' <span style="color:#777">(length: ' + args.length + ')</span>' : '');
-					}
-					else if (args instanceof window.RegExp)
-					{
-						return '[object RegExp]: ' + escapeHTML(args.toString());
-					}
-					else if (isEmpty(args))
-					{
-						var tmp;
-						return window.Object.prototype.toString.call(args) + (
-							args.valueOf ? ': ' + ((tmp = args.valueOf()) == args && (typeof tmp == 'object') ? ('length' in tmp ? '[]' : '{}') : tmp) : ''
-						);
-					}
-					else
-					{
-						var html = '<div style="display:block;color:black;background:white;"><label style="cursor:pointer;"><span style="font-family:sans-serif;font-size:13px;">&#9654;</span><input type="checkbox" style="display:none;" onchange="this.parentNode.parentNode.getElementsByTagName(\'div\')[0].style.display=this.checked?\'block\':\'none\';this.parentNode.getElementsByTagName(\'span\')[0].innerText=this.checked?\'&#9660;\':\'&#9654;\';">' + Object.prototype.toString.call(args) + '</label><div style="width:100%;border:1px solid #999;border-right:0px;' + (sub ? 'border-bottom:0px;' : '') + 'padding-left:6px;box-sizing:border-box;display:none;">';
-
-						for (var k in args)
-						{
-							html += '<pre style="' + pre_style + '"><span style="color:#F00;">' + k + '</span>: ' + generateHTML(args[k], true) + '</pre>';
-						}
-
-						return html + '</div></div>';
-					}
+					div.innerHTML += '<pre style="' + pre_style + '">' + (window.console._USE_JSON ? escapeHTML(JSON.stringify(args[i])) : generateHTML(args[i],false,true)) + '</pre>';
 				}
 
-			},
-
-			redirect_log = window.console.log = function ()
-			{
-
-				old_console_log.apply(window.console, arguments);
-
-				if (window.console._RELAY_TO_DOC)
+				if (window.console._RELAY_TO_DOC instanceof window.Element)
 				{
-					var div = window.document.createElement('div');
-
-					args = arguments;
-
-					div.style.border = '1px solid #333';
-					div.style.background = '#666';
-					div.className = 'console-log';
-					div.innerHTML = '<span style="color:#CDCDCD;font-family:sans-serif;font-size:13px;">Console message at ' + (new Date()) + '</span><b onclick="this.parentNode.parentNode.removeChild(this.parentNode)" style="float:right;color:#CDCDCD;cursor:pointer;">&times;</b>';
-
-
-					for (i = 0, l = args.length; i < l; i++)
-					{
-						div.innerHTML += '<pre style="' + pre_style + '">' + (window.console._USE_JSON ? escapeHTML(JSON.stringify(args[i])) : generateHTML(args[i])) + '</pre>';
-					}
-
-					if (window.console._RELAY_TO_DOC instanceof window.Element)
-					{
-						window.console._RELAY_TO_DOC.appendChild(div);
-					}
-					else
-					{
-						window.document.body.appendChild(div);
-					}
+					window.console._RELAY_TO_DOC.appendChild(div);
 				}
+				else
+				{
+					window.document.body.appendChild(div);
+				}
+			}
 
-			};
-
-		window.console._restore_old_log = function ()
-		{
-			window.console.log = old_console_log;
+		},
+		old_console_clear = window.console.clear,
+		redirect_clear = window.console.clear = function(){
+			old_console_clear.apply(window.console);
+			
+			for(var k in messages)
+			{
+				if(messages[k] && messages[k].parentNode)
+				{
+					messages[k].parentNode.removeChild(messages[k]);
+					messages[k] = null;
+				}
+			}
+			
+			messages = [];
 		};
 
-		window.console._restore_redirect_log = function ()
-		{
-			window.console.log = redirect_log;
-		};
+	window.console._restore_old_log = function ()
+	{
+		window.console.log = old_console_log;
+	};
+	window.console._restore_redirect_log = function ()
+	{
+		window.console.log = redirect_log;
+	};
 
-		window.addEventListener('error', function (e)
-		{
+	window.console._restore_old_clear = function ()
+	{
+		window.console.clear = old_console_clear;
+	};
+	window.console._restore_redirect_clear = function ()
+	{
+		window.console.log = redirect_clear;
+	};
 
-			var x = new Error(e.message);
+	window.addEventListener('error', function (e)
+	{
+		var x = new Error(e.message);
 
-			x.message = e.message;
-			x.filename = e.filename;
-			x.lineno = e.lineno;
-			x.colno = e.colno;
+		x.message = e.message;
+		x.filename = e.filename;
+		x.lineno = e.lineno;
+		x.colno = e.colno;
 
-			console.log(e.message, x);
-		});
+		console.log(e.message, x);
+	});
 
-	})(Function('return this')());
+})(Function('return this')());
